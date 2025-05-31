91cb365101f8bdd9b29ab9e839b9296960d9f4a2
ArkadiyGenij <agongadze8@gmail.com>
2025-05-31 12:25:21 +0300

    сделана верстка страницы FAQ, заменен шрифт, начата работа над модальным окном

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,20 @@ import {faq} from './faq.js';
 
 faq();
 
+new Swiper('.reviews__swiper', {
+  slidesPerView: 3,
+  autoHeight: true,
+  speed: 700,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true
+  },
+  navigation: {
+    prevEl: '.reviews__btn--prev',
+    nextEl: '.reviews__btn--next'
+  }
+});
+
 function scrollToSection(event) {
   event.preventDefault();
   const targetId = event.target.getAttribute('href').slice(1);
