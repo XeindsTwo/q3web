6d32a9a30c05382514b5cf4c80aefe83f0a41200
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2025-06-09 18:15:24 +0300

    сделан адаптив страницы с отзывами (reviews.html)

--- app/js/main.js
+++ app/js/main.js
@@ -4,20 +4,6 @@ import {modalRegistration} from "./modalRegistration.js";
 modalRegistration();
 faq();
 
-new Swiper('.reviews__swiper', {
-  slidesPerView: 3,
-  autoHeight: true,
-  speed: 700,
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true
-  },
-  navigation: {
-    prevEl: '.reviews__btn--prev',
-    nextEl: '.reviews__btn--next'
-  }
-});
-
 function scrollToSection(event) {
   event.preventDefault();
   const targetId = event.target.getAttribute('href').slice(1);
@@ -33,4 +19,28 @@ const menuLinks = document.querySelectorAll('.desktop');
 
 menuLinks.forEach((menuLink) => {
   menuLink.addEventListener('click', scrollToSection);
+});
+
+new Swiper('.reviews__swiper', {
+  autoHeight: true,
+  speed: 700,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true
+  },
+  navigation: {
+    prevEl: '.reviews__btn--prev',
+    nextEl: '.reviews__btn--next'
+  },
+  breakpoints: {
+    1160: {
+      slidesPerView: 3,
+    },
+    760: {
+      slidesPerView: 2,
+    },
+    320: {
+      slidesPerView: 'auto'
+    }
+  }
 });
